63281cc62628a9a70b57789cdbed66df60d9e6f1
nguyenkhanhngoc23 <ngocmq0707@gmail.com>
2025-02-09 09:29:37 +0700

    first commit

---
+++ bin/dart_application_basic.dart
@@ -0,0 +1,9 @@
+void main() {
+  int number1 = 20;
+  int number2 = 32;
+  var sum = number1 + number2;
+  var different = number1 - number2;
+  var product = number1 * number2;
+  var quotient = number1 / number2;
+  print("Tong cua ${number1} va ${number2} la ${sum}");
+}
---
+++ test/dart_application_basic_test.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  var name = 'voyager I';
+  var passExam = false;
+  var age = 20;
+  print(name.runtimeType);
+  print(age.runtimeType);
+  print(passExam.runtimeType);
+}
